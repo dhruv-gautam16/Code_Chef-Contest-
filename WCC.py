c181176e3403f53e900f4dc4fa180f87402bfd59
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-02-20 21:28:06 +0530

    Solution for WCC

---
+++ WCC.py
@@ -0,0 +1,15 @@
+T = int(input())
+for tc in range(T):
+	
+	x=int(input())
+	b=str(input())
+	a=b.count('C')
+	c=b.count('D')
+	d=b.count('N')
+	if a>d:
+		print(60*x)
+	elif a==d:
+		print(55*x)
+	elif d>a:
+		print(40*x)
+	
