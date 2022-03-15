dec279c9988d3bbc6c76ccd2ef2a96e8ec33ff8d
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-03-15 21:17:44 +0530

    Solution for CHFDBT

---
+++ CHFDBT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    n++;
+	    
+	    cout<<(n>>1)<<endl;
+	}
+	
+	return 0;
+}
