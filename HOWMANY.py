977d05f456a4e1550c792748bd44b9f2457e364a
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-04-03 21:39:27 +0530

    Solution for HOWMANY

---
+++ HOWMANY.py
@@ -0,0 +1,11 @@
+# cook your dish here
+n=int(input())
+s=str(n)
+if len(s) == 1 :
+    print("1")
+elif len(s) == 2:
+    print("2")
+elif len(s) == 3:
+    print("3")
+else:
+    print("More than 3 digits")
