874addcbb4eff9f4858e994c51ab60f269178814
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-05-19 22:57:06 +0530

    Solution for CHEFLUCK

---
+++ CHEFLUCK.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n = int(input())
+    f = 0
+    for i in range(7):
+        if (n - i * 4) < 0:
+            break  
+        if (n - i * 4) % 7 == 0:
+            f = 1 
+            print(n - i * 4)
+            break
+    if not f :
+        print(-1)# cook your dish here
