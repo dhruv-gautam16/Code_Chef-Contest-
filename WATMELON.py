c309982e0e32e1abbef8b940401a798ecb0ccb3f
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-06-13 12:55:07 +0530

    Solution for WATMELON

---
+++ WATMELON.py
@@ -0,0 +1,16 @@
+# cook your dish here
+'''for _ in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().split()))
+    if sum(l)<0:
+        print("no")
+    else:
+        print("yes")'''
+# cook your dish here
+for j in range(int(input())):
+    n = int(input())
+    a = list(map(int,input().split()))
+    if sum(a)<0:
+        print("NO")
+    else:
+        print("YES")
