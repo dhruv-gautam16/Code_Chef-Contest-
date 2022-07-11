47cfe0c50b8b6c26d3a5842bc7f23cba7e7fc8c0
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-07-12 01:50:38 +0530

    Solution for MGCSET

---
+++ MGCSET.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n,m=map(int,input().split())
+    a=[int(x) for x in input().split()]
+    c=0
+    for i in a:
+        if i%m==0:
+            c+=1
+    print(2**c-1)
