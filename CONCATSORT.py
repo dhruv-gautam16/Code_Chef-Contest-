e45409189a6673b6d5ab6ec68f29b8411fed5ff1
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-09-12 11:40:27 +0530

    Solution for CONCATSORT

---
+++ CONCATSORT.py
@@ -0,0 +1,24 @@
+# cook your dish here
+for _ in range(int(input())):
+    num = int(input())
+    array_1 = list(map(int,input().split()))[:num]
+    array_2 = sorted(array_1)
+    array_3 = [array_1.index(array_2[0]),0]
+    null_array_1 = [0]*num
+    # loop:
+    for i in range(2):
+        array_3[0] = 0
+        # while condition:
+        while array_3[0] < num:
+            # checking not same:
+            if not null_array_1[array_3[0]] and array_1[array_3[0]] == array_2[array_3[1]]:
+                null_array_1[array_3[0]] = 1
+                array_3[0] += 1
+                array_3[1] += 1
+            else:
+                array_3[0] += 1
+    # current check:
+    if array_3[0] == array_3[1]:
+        print("YES")
+    else:
+        print("NO")
