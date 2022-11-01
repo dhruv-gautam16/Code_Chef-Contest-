6db8a06f6ebe4aadfa31261a51d101c31366237c
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-11-01 11:39:00 +0530

    Solution for HEADBOB

---
+++ HEADBOB.py
@@ -0,0 +1,10 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+    s=input()
+    if 'I' in s:
+        print('INDIAN')
+    elif 'Y' in s:
+        print('NOT INDIAN')
+    else:
+        print('NOT SURE')
