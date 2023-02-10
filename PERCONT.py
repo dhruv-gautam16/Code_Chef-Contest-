a21c61a59cca7bd26cb5701f07e63bdff0e0664a
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-02-10 22:24:07 +0530

    Solution for PERFCONT

---
+++ PERCONT.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+
+while T:
+    N, P = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    e = 0
+    h = 0
+    for i in A:
+        if i>=P//2:
+            e+=1
+        elif i<=P//10:
+            h+=1
+    if P==1:
+        print("no")
+    elif e==1 and h==2:
+        print("yes")
+    else:
+        print("no")
+            
+    T = T-1
