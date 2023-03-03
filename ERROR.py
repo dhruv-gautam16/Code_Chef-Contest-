1598cea740d92fe702e5728494eae9f8449fea82
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-03-03 12:26:53 +0530

    Solution for ERROR

---
+++ ERROR.py
@@ -0,0 +1,11 @@
+# cook your dish here
+t= int(input())
+while t>0:
+    s = input ()
+    if "010" in s:
+        print("Good")
+    elif "101" in s:
+        print("Good")
+    else:
+        print("Bad")
+    t -= 1
