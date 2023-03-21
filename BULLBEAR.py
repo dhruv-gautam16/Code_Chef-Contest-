77fd5024fae4ebbd97822faaa0dd5da890e4fab4
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-03-21 09:11:17 +0530

    Solution for BULLBEAR

---
+++ BULLBEAR.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    x,y = map(int,input().split())
+    if(x>y):
+        print("LOSS")
+    elif(x<y):
+        print("PROFIT")
+    else:
+        print("NEUTRAL")
