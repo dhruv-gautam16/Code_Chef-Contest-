eb40e5d25d5edb77b7a1f5daacc1196570722434
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-03-24 09:13:49 +0530

    Solution for FINE

---
+++ FINE.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for t in range(t):
+    x=int(input())
+    if(x<=70):
+        print("0")
+    elif(x>70 and x<=100):
+        print("500")
+    else:
+        print("2000")
--- ONEMORE.py
+++ ONEMORE.py
@@ -5,3 +5,4 @@ for i in range(t):
         print("yes")
     else:
         print("no")
+FINE
