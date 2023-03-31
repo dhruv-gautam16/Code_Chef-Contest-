ad0fad6bbdff09c320715350c2556df3331823e1
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-03-31 15:01:34 +0530

    Solution for ADVANCE

---
+++ ADVANCE.py
@@ -0,0 +1,8 @@
+# cook your dish here
+n=int(input())
+for i in range(n):
+    l=list(map(int,input().split()))[:2]
+    if l[1] in range(l[0],l[0]+201) :
+        print('YES')
+    else:
+        print('NO')
