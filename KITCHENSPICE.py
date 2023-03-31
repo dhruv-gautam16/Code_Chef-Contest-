2cb8bbe4fee96d3e606f6782b52389da02a3fa68
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-03-31 15:02:19 +0530

    Solution for KITCHENSPICE

---
+++ KITCHENSPICE.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    x=int(input())
+    if(x<4):
+        print("MILD")
+    elif(x>=4 and x<7):
+        print("MEDIUM") 
+    else:
+        print("HOT")
