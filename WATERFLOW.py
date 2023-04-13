cb6185f5d0c3431e94e955bcdc5faaf30e188099
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-13 09:04:37 +0530

    Solution for WATERFLOW

---
+++ WATERFLOW.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    w,x,y,z=map(int,input().split())
+    if w+y*z>x:
+        print("Overflow")
+    elif w+y*z<x:
+        print("unfilled")
+    else:
+        print("Filled")
