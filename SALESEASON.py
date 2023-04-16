12a1717538e3a04e3541b3e21fb9cff8c8aee5df
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-16 22:00:35 +0530

    Solution for SALESEASON

---
+++ SALESEASON.py
@@ -0,0 +1,15 @@
+# cook your dish here
+t=int(input())
+for t in range(t):
+    x=int(input())
+    if(x<=100):
+        print(x)
+    elif(x>100 and x<=1000):
+        print(x-25)
+    elif(x>1000 and x<=5000 ):
+        print(x-100)
+    elif(x>5000):
+        print(x-500)
+    else:
+        print(" ")
+        
