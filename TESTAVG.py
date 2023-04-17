4b08b60e51b637bd86368b91810390f913f88e04
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-17 09:08:39 +0530

    SOlution for TESTAVG

---
+++ TESTAVG.py
@@ -0,0 +1,11 @@
+# cook your dish here
+"""
+hello sir
+"""
+hlo=int(input())
+for love in range(hlo):
+    l=list(map(int,input().split()))
+    if (l[0]+l[1])/2>=35 and (l[2]+l[1])/2>=35 and (l[0]+l[2])/2>=35 :
+        print("pass")
+    else:
+        print("fail")
