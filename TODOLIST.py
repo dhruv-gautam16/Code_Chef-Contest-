e57292161d57b1e5dddcbc5ce99cb28c8e03c23f
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-19 10:24:36 +0530

    Solution for TODOLIST

---
+++ TODOLIST.py
@@ -0,0 +1,11 @@
+# cook your dish here
+T = int(input())
+for i in range(T):
+    N = int(input())
+    D = list(map(int,input().split()))
+    count = 0
+    for i in range(N):
+        if D[i]>=1000:
+            count = count+1
+            continue
+    print(count)
