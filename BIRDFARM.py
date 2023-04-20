478052b115c6bfff1b9bcfca6edb0a42efa425c1
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-20 23:25:27 +0530

    Solution for BIRDFARM

---
+++ BIRDFARM.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t = int(input())
+for i in range(t):
+    x,y,z = map(int, input().split())
+    if (z%x == 0 and z%y != 0):
+        print("CHICKEN")
+    elif (z%y == 0 and z%x != 0):
+        print("DUCK")
+    elif (z%x == 0 and z%y == 0):
+        print("ANY")
+    else:
+        print("NONE")
