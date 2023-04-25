6d269d31692ce1e66f639ffe9a1db8381bd99ef9
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-26 01:34:08 +0530

    Solution for LASTLEVELS

---
+++ LASTLEVELS.py
@@ -0,0 +1,15 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    
+    x,y,z=map(int,input().split())
+    a=x*y
+    b=x//3
+    if x%3==0:
+        b-=1
+        print(a+b*z)
+    else:
+        print(a+b*z)
+
+        
+
