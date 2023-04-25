9b7cec7f48ec3d260d8215a52bfb27429f91f415
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-25 10:48:33 +0530

    Solution for LUCKYN

---
+++ LUCKYN.py
@@ -0,0 +1,9 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+        x=int(input())
+        x=str(x)
+        if "7" in x:
+                print("YES")
+        else:
+                print("NO")
