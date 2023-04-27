5b92f6d25be4aae49fea9f8d85cc6c00a4c11121
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-28 00:16:53 +0530

    Solution for MYSERVE

---
+++ MYSERVE.py
@@ -0,0 +1,11 @@
+# cook your dish here
+t = int(input())
+
+for i in range(t):
+    p,q = map(int, input().split())
+    a = p+q
+    if a % 4 == 1 or a % 4 ==0:
+        print('Alice')
+    else:
+        print('Bob')
+        
