b1dbe6a67a369199d2b7ac30b66d3833ce71665a
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-04-29 01:32:04 +0530

    Solution for CHEFAPPS

---
+++ CHEFAPPS.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+        s,x,y,z=map(int,input().split())
+        if (x+y)+z<=s:
+                print("0")
+        elif (x+z)<=s or (y+z)<=s:
+                print("1")
+        else:
+                print("2")
