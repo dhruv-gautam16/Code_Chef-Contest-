1fea457875157d19546ab8e362f39e43b6b06d09
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-05-01 00:13:55 +0530

    Solution for MATDIF

---
+++ MATDIF.py
@@ -0,0 +1,14 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    #a=list(map(int,input().split()))
+    m=2
+    for i in range(n):
+        for j in range(n):
+            if m<=(n*n):
+                print(m,end=" ")
+            else:
+                m=1
+                print(m,end=" ")
+            m+=2
+        print()
