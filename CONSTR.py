fb07675f5f246ff21f5eda36f2f6866081f669f2
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-05-01 00:17:06 +0530

    Solution for CONSTR

---
+++ CONSTR.py
@@ -0,0 +1,18 @@
+for _ in range(int(input())):
+    N = int(input())
+    In = input()
+    res,count = "",1
+    for i in range(1,N):
+        if In[i] == In[i-1]:
+            count += 1
+        else:
+            if(count%2 == 1):
+                res += In[i-1]
+            else:
+                res = res+(In[i-1]*2)
+            count = 1
+    if count%2 == 1:
+        res += In[N-1]
+    else:
+        res += (In[N-1]*2)
+    print(res)
