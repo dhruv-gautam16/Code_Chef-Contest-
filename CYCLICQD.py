43efc7791502db132858b2d420a5d273388ef0f0
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-05-02 01:08:21 +0530

    Solution for CYCLICQD

---
+++ CYCLICQD.py
@@ -0,0 +1,9 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    a,b,c,d=map(int,input().split())
+    if(a+c==180)and(b+d==180):
+        print("yes")
+    else:
+        print("no")
+    
