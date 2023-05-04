c29b799115f1670f10a87e7d0d7b3998c3a82adf
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-05-04 11:36:20 +0530

    Solution for FARFROMO

---
+++ FARFROMO.py
@@ -0,0 +1,11 @@
+for t in range(int(input())):
+    x1, y1, x2, y2 = map(int, input().split())
+    alex = pow(x1, 2) + pow(y1, 2)
+    bob = pow(x2, 2) + pow(y2, 2)
+        
+    if(alex > bob):
+        print("Alex")
+    elif(bob > alex):
+        print("Bob")
+    elif(alex == bob):
+        print("Equal")
