79a179c3031868875cefa9a55d57b65ed392aaa2
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-05-29 11:20:56 +0530

    Solution for EQUALELE

---
+++ EQUALELE.py
@@ -0,0 +1,12 @@
+goals = int(input())
+for distractions in range(goals):
+    wife = int(input())
+    life = list(map(int, input().split()))
+    aim = {}
+    for death in life:
+        if death not in aim:
+            aim[death] = 1
+        else:
+            aim[death] += 1
+    stress = max(aim.values())
+    print(len(life) - stress)
