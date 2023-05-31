50f0d2ad5098b79bb53aa162ac1af359b80a4e5d
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-05-31 14:50:56 +0530

    Solution for RETURNCHANGE

---
+++ RETURNCHANGE.py
@@ -0,0 +1,11 @@
+import math
+t=int(input())
+while t!=0:
+    n=int(input())
+    if n%10>=5:
+        r= math.ceil(n/10)*10
+    else:
+        r=math.floor(n/10)*10
+    print(100-r)
+    t-=1
+    
