7e88c9b98042100ea635a0b726db88cfa93a1418
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-05-31 14:51:35 +0530

    Solution for FOODPLAN

---
+++ FOODPLAN.py
@@ -0,0 +1,11 @@
+t=int(input())
+for i in range(0,t):
+    n,m=[int(x) for x in input().split()]
+    onl=n/10
+    k=n-onl
+    if(m<k):
+        print("DINING")
+    elif(k<m):
+        print("ONLINE")
+    else:
+        print("EITHER")
