64797b32a87245ce1553dc255d80d3c6899f54b7
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-02 16:44:23 +0530

    Solution for FAVOUORITENUM

---
+++ FAVOURITENUM.py
@@ -0,0 +1,7 @@
+# cook your dish here
+fav = int(input())
+for f in range(fav):
+  num = int(input())
+  if num%14==0: print("alice")
+  elif num%9==0 and num%2==1: print("bob")
+  else: print("charlie")
