d7e736d453daf53ef79f253687a62bb1df06b00d
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-03 10:41:19 +0530

    Solution for CHN15A

---
+++ CHN15A.py
@@ -0,0 +1,8 @@
+# cook your dish here
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    minions = list(map(int, input().split()))
+    count = sum(1 for minion in minions if (minion + k) % 7 == 0)
+    print(count)
