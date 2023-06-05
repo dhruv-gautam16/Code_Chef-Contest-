28aafd74dd86f9f8c62fecaa05f4894ba01ba351
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-05 13:03:01 +0530

    Commit

--- PRB01.py
+++ PRB01.py
@@ -1,20 +1,4 @@
-# cook your dish here
-# t = int(input())
-# for i in range(t):
-#     n = int(input())
-#     prime = True
-#     if n==1:
-#         print("yes")
-#     for i in range(2,n):
-#         if(n%i==0):
-#             prime = False
-#             break
-        
-    # if prime :
-    #     print("yes")
-    # else:
-    #     print("no")
-        
+ 
 for i in range(int(input())):
     a=int(input())
     m=0
