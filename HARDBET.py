e019f75d2f793ca63d369c445864b939b6e7da30
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-07 09:57:15 +0530

    Solution for HARDBET

---
+++ HARDBET.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    submissions = list(map(int,input().split()))
+    hardest = min(submissions)
+    if hardest is submissions[2]:
+        print("Alice")
+    elif hardest is submissions[1]:
+        print("Bob")
+    else:
+        print("Draw")
+    
