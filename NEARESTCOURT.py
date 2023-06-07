11e641eea821cf4174e4a613b4f9e7dc599e6383
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-07 10:01:04 +0530

    Solution for NEARESTCOURT

---
+++ NEARESTCOURT.py
@@ -0,0 +1,9 @@
+# cook your dish here
+t = int(input())
+
+for _ in range(t):
+    x, y = map(int, input().split())
+    mid = (x+y)//2
+    dist1 = abs(mid-x)
+    dist2 = abs(mid-y)
+    print(max(dist1, dist2))
