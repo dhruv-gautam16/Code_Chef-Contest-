296d0f2b944953a85ebd0acb50c9a0a65bfbedfd
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-08 10:37:49 +0530

    Solution for SEATNUMBER

---
+++ SEATNUMBER.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t = int(input())
+for i in range(t):
+    n = int(input())
+    if 1<=n<=10:
+        print("lower double")
+    elif 11<=n<=15:
+        print("lower single")
+    elif 16<=n<=25:
+        print("upper double")
+    else:
+        print("upper single")
