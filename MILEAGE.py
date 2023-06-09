04e587078f1927256878a46272d341eb91929c28
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-09 23:00:58 +0530

    Solution for MILEAGE

---
+++ MILEAGE.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n,x,y,a,b=map(int,input().split())
+    p=(n/a)*x
+    d=(n/b)*y
+    if(p<d):
+        print("PETROL")
+    elif(p==d):
+        print("ANY")
+    else:
+        print("DIESEL")
