f6ce9d0199ed3097ec9700846f334821c7eafe5a
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-09 23:00:03 +0530

    Solution for GOODWEAT

---
+++ GOODWEAT.py
@@ -0,0 +1,14 @@
+# cook your dish here
+for t in range(int(input())):
+    a=list(map(int,input().split()))
+    i=0
+    j=0
+    for k in range(len(a)):
+        if(a[k]==1):
+            i=i+1
+        else:
+            j=j+1
+    if(i<j):
+        print("NO")
+    else:
+        print("YES")
