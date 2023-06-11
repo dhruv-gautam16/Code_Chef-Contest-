56e9c314ab14149d21926f0c1d7d3afe667ec463
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-11 12:42:50 +0530

    Solution for EQUALISE

---
+++ EQUALISE.py
@@ -0,0 +1,17 @@
+# cook your dish here
+for i in range(int(input())):
+    a, b = map(int, input().split(" "))
+    if a == b:
+        print("Yes")
+    else:
+        # check if the number can be achieved or not 
+        mini = min(a,b)
+        maxi = max(a,b)
+        
+        while(mini<maxi):
+            mini *= 2
+            
+        if mini == maxi:
+            print("Yes")
+        else:
+            print("No")
