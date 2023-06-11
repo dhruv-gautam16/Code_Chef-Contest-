ee33071c06675c86b0b19753a71d1e4688909acb
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-11 12:44:28 +0530

    Solution for BMI

---
+++ BMI.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    c=(a/(b**2))
+    if c<=18:
+        print(1)
+    elif c>=30:
+        print(4)
+    elif c>18 and c<25:
+        print(2)
+    elif c>24 and c<30:
+        print(3)
