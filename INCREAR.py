ba1d352467d0c80047771935a26704d13a9f2923
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-12 10:47:36 +0530

    Solution for INCREAR

---
+++ INCREAR.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if x<y:
+        print(y-x)
+    else:
+        if (x-y)%2==0:
+            print((x-y)//2)
+        else:
+            print((x-y)//2+2)
+    
