7f2b18cbee1ec7d26010c9efb385083d6083123d
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-15 11:28:33 +0530

    Solution for WHICHDIV

---
+++ WHICHDIV.py
@@ -0,0 +1,10 @@
+# cook your dish here
+n=int(input())
+for i in range(n):
+    a=int(input())
+    if 2000<=a:
+        print("1")
+    elif 1600<=a<2000:
+        print("2")
+    elif a<1600:
+        print("3")
