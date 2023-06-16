932090fd61845db24c65d4fe2cced011fa028c55
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-16 19:37:43 +0530

    Solution for DRUNKALK

---
+++ DRUNKALK.py
@@ -0,0 +1,14 @@
+t=int(input())
+for i in range(t):
+    k=int(input())
+    s=1
+    c=0
+    while s<=k:
+        if s%2!=0:
+            c=c+3
+        else:
+            c=c-1
+        s=s+1
+    print(c)
+            
+        
