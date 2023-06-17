f6e500ed3efc8d3a54cf28e382cff5f792dbebdc
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-17 22:47:40 +0530

    Solution for CCISLAND

---
+++ CCISLAND.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range(int(input())):
+    x,y,xr,yr,D=map(int,input().split())
+    a=x//xr
+    b=y//yr
+    av=min(a,b)
+    if av>=D:
+        print("YES")
+    else:
+        print("NO")
+    
