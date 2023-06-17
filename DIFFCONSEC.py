a95898d0bd95cf89ab76702194e1551649998c0b
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-17 22:46:54 +0530

    Solution for DIFFCONSEC

---
+++ DIFFCONSEC.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for i in range(0,t):
+    n=int(input())
+    s=input()
+    count=0
+    for i in range(0,n-1):
+        if(s[i]==s[i+1]):
+            count=count+1
+    print(count)
