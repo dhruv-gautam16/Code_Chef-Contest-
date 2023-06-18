ec2a44aa10196024c2dea93ec5cbd99b1f42d1aa
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-18 11:20:07 +0530

    Solution for CHEFRUN

---
+++ CHEFRUN.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range(int(input())):
+  X1,X2,X3,V1,V2 = map(int,input().split())
+  S1 = (X3-X1)/V1
+  S2 = (X2-X3)/V2
+  if S1 > S2:
+    print("Kefa")
+  elif S1 < S2:
+    print("Chef")
+  else:
+    print("Draw")
