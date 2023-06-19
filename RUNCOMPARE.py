dbd19aafc7c04feaecf524132e5aa05892842eb1
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-19 15:29:14 +0530

    Solution for RUNCOMPARE

---
+++ RUNCOMPARE.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    a=list(map(int , input().split()))
+    b=list(map(int , input().split()))
+    ans = 0 
+    for i in range(n):
+        if a[i]<=2*b[i] and b[i]<=2*a[i]:
+            ans+=1 
+    print(ans)
