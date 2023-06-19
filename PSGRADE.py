dcda5506c4934091f41293d263fb5961c20d9a2d
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-20 00:46:48 +0530

    Commit

---
+++ PSGRADE.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b,c,r,x,y,z=map(int,input().split())
+    if((x+y+z)>=r and x>=a and y>=b and z>=c):
+        print('YES')
+    else:
+        print('NO')
