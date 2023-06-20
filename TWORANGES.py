aad6453e0454c94463ac2faf953e1276847d82b0
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-21 00:41:48 +0530

    Solution for TWORANGES

---
+++ TWORANGES.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    a,b,c,d=map(int, input ().split())
+    x=[]
+    for i in range(a,b+1):
+        x.append(i)
+    for j in range(c,d+1):
+        x.append(j)
+    print (len(set(x)))
