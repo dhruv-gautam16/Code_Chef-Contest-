842fd6caabbec77b1c91af64b875fd7a3772d439
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-22 13:25:21 +0530

    Solution for PENALTY

---
+++ PENALTY.py
@@ -0,0 +1,18 @@
+t = int(input())
+for i in range(t):
+  a = list(map(int,input().split(' ')))
+  count = 0
+  count1 = 0
+  for j in range(len(a)):
+    if j%2 == 0:
+      if a[j] == 1:
+        count+=1
+    else:
+      if a[j] == 1:
+        count1+=1
+  if count > count1:
+    print(1)    
+  elif count == count1:
+   print(0) 
+  else:
+   print(2)
