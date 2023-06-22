74f6c4c031e19d8873eb01e213587216575529d6
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2023-06-22 13:26:51 +0530

    Solution for PROBLEM

---
+++ PROBLEM.py
@@ -0,0 +1,20 @@
+for _ in range(int(input())):
+    n,m=map(int,input().split())
+    k=0
+    while(m>0 and n>0):
+        if m==n:
+            k=1
+            break
+        elif abs(m-n)==1:
+            k=0
+            break
+        elif m>n:
+            m=m-1
+            n=n+1
+        else:
+            n=n-1
+            m=m+3
+    if k==1:
+        print("YES")
+    else:
+        print("NO")
